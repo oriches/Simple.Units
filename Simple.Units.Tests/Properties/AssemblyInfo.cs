23da2ed40903c742a7b667328a9bfc2489a3b526
Ollie Riches <ollie.riches@btinternet.com>
2016-07-28 23:02:34 +0100

    Updated nuget project name

--- Simple.Units.Tests/Properties/AssemblyInfo.cs
+++ Simple.Units.Tests/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Simple.Units.Fixtures")]
+[assembly: AssemblyTitle("Simple.Units.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Simple.Units.Fixtures")]
+[assembly: AssemblyProduct("Simple.Units.Tests")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
